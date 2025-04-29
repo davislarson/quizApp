147167647e8cf6d3297b8379a3da825116cc9488
davissl <davissl@byu.edu>
2025-04-29 15:52:57 -0600

    some refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
+<script setup>
+import Card from './components/Card.vue';
+import q from './data/quizzes.json'
+import { ref, watchEffect } from 'vue'
+
+const quizzes = ref(q);
+const search = ref('');
+
+watchEffect(() => {
+  if (search.value) {
+    quizzes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
+  }
+  else {
+    quizzes.value = q;
+  }
+})
+
+</script>
+
+
 <template>
   <div class="container">
     <header>
       <h1>Quizzes</h1>
-      <input type="text" placeholder="Search...">
+      <input v-model.trim="search" type="text" placeholder="Search...">
     </header>
     <div class="options-container">
-      <div class="card">
-        <img src="./assets/math.png" alt="">
-        <div class="card-text">
-          <h2>Math</h2>
-          <p>15 Questions</p>
-        </div>
-      </div>
-    
+      <Card :quizzes="quizzes"/>
     </div>
   </div>
 </template>
@@ -43,28 +56,5 @@ header input {
   display: flex;
   flex-wrap: wrap;
   margin-top: 40px;
-}
-
-/* CARD */
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(196, 195, 195, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-.card-text {
-  padding: 0 5px;
-}
-
-.card h2 {
-  font-weight: bold;
 }
 </style>
